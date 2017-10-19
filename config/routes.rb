297dd549a4be1e7b3188c405360d03abffec649c
Nmuta Jones <nmuta1@gmail.com>
2017-10-19 15:09:47 -0700

    add categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :categories
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :fruits
 
   get 'get_fruits' => 'fruits#get_fruits'
+  get 'get_categories' => 'categories#get_categories'
+
+
   get 'forbidden' => 'pages#forbidden'
 
 
